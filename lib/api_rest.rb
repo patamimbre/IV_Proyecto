fb0c8038bfa1e8cb38c3fdb17c1e843255331532
Germán Castro <germancastromontes@gmail.com>
2017-11-03 17:16:26 +0100

    closes #18 closes #20

--- lib/api_rest.rb
+++ lib/api_rest.rb
@@ -1,2 +1,30 @@
-class ApiRest
+require 'sinatra'
+require 'json'
+
+get '/' do
+  {'status' => 'ok',
+   'ejemplo' =>
+       {'ruta' => "/search/CVE-test",
+        'valor' => {'status'=>'bad_id'}.to_json
+       }}.to_json
 end
+
+get '/search/:id' do |id|
+  if id !~ /\ACVE-\d{4}-\d{4}\z/
+    {'status' => 'bad_id'}.to_json
+
+  else
+
+    # buscar cve en DB y devolver json
+    "CVE -> #{id}"
+
+  end
+end
+
+get '/service/:service' do |s|
+
+  # buscar cves en db por nombre de servicio
+  "Servicio -> #{s}"
+
+end
+
